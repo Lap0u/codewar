ddbf8ac1153f805f2d970b568e9addf9f08f3c40
Clement Beaurain <clementbeaurainpro@gmail.com>
2022-12-31 13:50:44 +0100

    gerer le cas ou il y a une boucle infinie de parents

--- 4kyu/pathfinder_1.js
+++ 4kyu/pathfinder_1.js
@@ -1,7 +1,3 @@
-function updateDistance(Point, maze) {
-  Point.toEnd = maze.length - Point.x + maze[0].length - Point.y
-}
-
 class Point {
   constructor(x, y, value) {
   this.x = x
@@ -23,6 +19,7 @@ class Point {
     let currJ = this.y
     if (this.value === 'W')
       return
+    console.log('t', currI, currJ);
     if (currI > 0 && allPoints[currI- 1][currJ].value !== 'W')
       this.children.push(allPoints[currI- 1][currJ])
     if (currJ > 0 && allPoints[currI][currJ - 1].value !== 'W')
@@ -51,19 +48,25 @@ function initAllPoints(maze) {
   return res
 }
 
-function findNextBest(currentPoint) {
+function findNextBest(currentPoint, visited) {
+  // console.log('fnb');
+  i = 0;
   while (currentPoint.children.length === 0) {
+    i++
+    if (i > 1000)
+      return false
+    // console.log('loop', currentPoint);
     currentPoint = currentPoint.parent
     if(currentPoint === undefined)
       return false
   }
+  // console.log('find', currentPoint)
   lowPoint = currentPoint.children[0]
   for (i = 0; i < currentPoint.children.length; i++) {
-    if (currentPoint.children[i].toEnd < lowPoint.toEnd && currentPoint.children[i].children.length !== 0)
+    if (currentPoint.children[i].toEnd < lowPoint.toEnd)
       lowPoint = currentPoint.children[i]
   }
-  console.log('low', lowPoint);
-  return lowPoint
+  return [lowPoint, currentPoint]
 }
 
 function removeChildren(currentPoint, nextPoint) {
@@ -82,22 +85,23 @@ function startSearch(allPoints, maze) {
   mazeHeight = maze.length
   current = allPoints[0][0]
   while (current !== allPoints[mazeHeight -1][mazeWidth - 1]) {
-      next = findNextBest(current)
-      // console.log(('nn', next));
+      res = findNextBest(current)
+      next = res[0]
+      current = res[1]
+      if(next === undefined || current === undefined)
+        return false;
+      // console.log('parent', current)
+      // console.log('testing', next)
       if (typeof next === "boolean") {
-        console.log("End", next)
+        // console.log("End", next)
         return next
       }
       next.stepsDone = current.stepsDone + 1
       next.parent = current
       removeChildren(current, next)
+      // console.log('parent 2', current)
+      // console.log('testing 2', next)
       current = next
-      if (current.x === 2 || current.y === 2)
-      {
-        console.log('fon', current);
-        return false
-      }
-      // return false
   }
   return true
 }
@@ -113,15 +117,21 @@ function testMaze(expected, maze){
   console.log(expected === actual)
 }
 
-// testMaze(true,
-// `.W.
-// .W.
-// ...`);
+testMaze(true,
+ `.W...W...
+  .......W.
+  WW.....W.
+  ..W....WW
+  WW..WW...
+  WW...W...
+  ...W.W...
+  ....W.W..
+  W..W.....`);
 
-testMaze(false,
-`...
-.W.
-W..`);
+// testMaze(false,
+// `..W
+// .W.
+// W..`);
 
 // testMaze(true,
 // `......
